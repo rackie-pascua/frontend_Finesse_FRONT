3d118840db0a1a0cd5d7751be9a5d98170df3f8d
thomkainos <thomas.phan@kainos.com>
2024-07-11 13:26:43 +0100

    last linter comment

--- src/services/HRService.ts
+++ src/services/HRService.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 import { Employee } from "../models/Employee";
 // import { getHeader } from "./AuthUtils";
 
-export const createDeliveryEmployee = async (employee: Employee): Promise<Number> => {
+export const createDeliveryEmployee = async (employee: Employee): Promise<number> => {
     try {
         const response: AxiosResponse = await axios.post("http://localhost:8080/api/HR", employee);
         
