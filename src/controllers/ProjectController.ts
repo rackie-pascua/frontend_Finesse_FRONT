5b3566c5227f17d5d55f6e8d8bc9221db4bd276b
rackie-pascua <rackie.pascua@kainos.com>
2024-07-11 12:18:13 +0100

    Creating Project Form

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProject, getProjectById } from "../services/ProjectService"
+import { createProject } from "../services/ProjectService"
 
 export const getProjectForm = async (req: express.Request, res: express.Response): Promise<void> => {
     res.render('projectForm.html');
@@ -8,12 +8,9 @@ export const getProjectForm = async (req: express.Request, res: express.Response
 export const postProjectForm = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
         const id = await createProject(req.body);
-        res.redirect('/projects/' + id);
+        res.render('project.html');
     } catch (e) {
         res.locals.errormessage = e.message;
         res.render('projectForm.html', req.body);
     }
-}
-export const getSingleProject = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('projectDetail.html',  {product: await getProjectById(req.params.id) });
 }
