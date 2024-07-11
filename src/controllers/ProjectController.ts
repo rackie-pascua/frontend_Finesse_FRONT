7f55d6083160d1aa08227f601e2e05f4661d3fa9
rackie-pascua <rackie.pascua@kainos.com>
2024-07-11 15:38:17 +0100

    dropdown for client

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,16 +1,18 @@
 import express from "express";
-import { createProject } from "../services/ProjectService"
+import { createProject, getAllProjects } from "../services/ProjectService"
 
 export const getProjectForm = async (req: express.Request, res: express.Response): Promise<void> => {
-    res.render('projectForm.html');
+    res.render('projectForm.html', { clients: await getAllProjects() });
 }
 
 export const postProjectForm = async (req: express.Request, res: express.Response): Promise<void> => {
     try {
         await createProject(req.body);
         res.render('project.html');
-    } catch (e) {
+    } catch(e) {
         res.locals.errormessage = e.message;
         res.render('projectForm.html', req.body);
     }
 }
+
+
--- src/services/ProjectService.ts
+++ src/services/ProjectService.ts
@@ -1,10 +1,23 @@
 import axios, { AxiosResponse } from "axios";
 import { ProjectRequest } from "../models/ProjectRequest";
+import { ProjectResponse } from "../models/ProjectResponse";
 
 
 export const createProject = async (project: ProjectRequest): Promise<number> => {
     try {
         const response: AxiosResponse = await axios.post("http://localhost:8080/api/management/", project);
+
+        return response.data;
+    } catch (e) {
+        console.log(e);
+        throw new Error(e.response.data);
+    }
+}
+
+
+export const getAllProjects = async (): Promise<ProjectResponse[]> => {
+    try {
+        const response: AxiosResponse = await axios.get("http://localhost:8080/api/management/");
 
         return response.data;
     } catch (e) {
