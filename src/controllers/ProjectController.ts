e75c5baf6653235bd7df39731c6b84d2006eff98
rackie-pascua <rackie.pascua@kainos.com>
2024-07-11 16:08:41 +0100

    dropdown for client

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -9,7 +9,7 @@ export const getProjectForm = async (req: Request, res: Response): Promise<void>
 export const postProjectForm = async (req: Request, res: Response): Promise<void> => {
     try {
         await createProject(req.body);
-        res.render('project.html', getAllClients());
+        res.render('project.html');
     } catch(e) {
         res.locals.errormessage = e.message;
         res.render('projectForm.html', req.body);
