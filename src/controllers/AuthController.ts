b2a01ebbcb5bfd2a1c63c0691a0a190db897bcf5
ashleighB31 <ashleigh.brennan@kainos.com>
2024-07-11 15:37:09 -0700

    Adding auth changes

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import { getToken, registerNewUser }  from "../services/AuthService"
+
+
+export const getLoginForm = async (req: express.Request, res: express.Response): Promise<void> => {
+    res.render('loginForm.html');
+}
+
+export const postLoginForm = async (req: express.Request, res: express.Response): Promise<void> => {
+    try {
+        req.session.token = await getToken(req.body);
+        res.redirect('/orders');
+        
+    } catch (e) {
+        res.locals.errormessage = e.message;
+        res.render('loginForm.html', req.body);
+    }
+}
+
+export const getRegisterForm = async (req: express.Request, res: express.Response): Promise<void> => {
+    res.render('registerForm.html');
+}
+
+export const postRegisterForm = async (req: express.Request, res: express.Response): Promise<void> => {
+try {
+    await registerNewUser(req.body);
+    res.redirect('/loginForm')
+} catch (e) {
+    res.locals.errormessage = e.message;
+        res.render('registerForm.html', req.body);
+}
+}
